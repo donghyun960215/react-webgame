6bcbea08d1459a6cf3157d89ea6be8c00537f02e
donghyun960215 <tlzma789@naver.com>
2023-05-16 08:45:50 +0900

    23.05.16 출근 전 공부

--- 구구단/client.jsx
+++ 구구단/client.jsx
@@ -1,7 +1,8 @@
 const React = require('react');
-const ReactDOM = require('react-dom');
-
+const ReactDOM = require('react-dom/client');
 const GuGuDan = require('./GuGuDan');
 
-ReactDOM.render(<GuGuDan/ >, document.querySelector('#root'));
+
+//ReactDOM.render(<GuGuDan/ >, document.querySelector('#root'));
+ReactDOM.createRoot(document.querySelector('#root')).render(<GuGuDan />);
 
