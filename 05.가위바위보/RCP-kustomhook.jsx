8c593690e1de4f2488f37f5b32ad85185a58d5d8
donghyun960215 <tlzma789@naver.com>
2023-05-24 08:39:10 +0900

    23.05.24 출근 전 공부

---
+++ 05.가위바위보/RCP-kustomhook.jsx
@@ -0,0 +1,91 @@
+import React, {useState, useRef, useEffect} from "react";
+import useInterval from "./useInterval";
+
+//클래스의 경우 -> constructor -> render -> ref -> componentDidMount -> 
+//          (setState/prpos 바뀔때 -> shouldComponentUpdate(true) -> render -> componentDidUpdate)
+//부모가 나를 없앴을 떄 -> componentWillUnmount -> 소멸
+
+const rspCoords = {
+  바위 : '0',
+  가위 : '-142px',
+  보: '-284px'
+};
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1
+};
+const comuterChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function(v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+const RSP = () => {
+  const [result, setResult] = useState('');
+  const [imgCoord, setImgCoord] = useState(rspCoords.바위);
+  const [score, setScore] = useState(0);
+  const [fullCount, setFullCount] = useState(10);
+  const [isRunning, setIsRunning] = useState(true);
+
+  const changHand = () => {
+      if(imgCoord === rspCoords.바위) {
+        setImgCoord(rspCoords.가위)
+      }else if(imgCoord === rspCoords.가위) {
+        setImgCoord(rspCoords.보)
+      }else if(imgCoord === rspCoords.보) {
+        setImgCoord(rspCoords.바위)
+      }
+  }
+
+  useInterval(changHand, isRunning ? 100 : null);
+
+  const onClickBtn = (choice) => () => {
+    if (isRunning) {
+      setIsRunning(false);
+      const myScore = scores[choice];
+      const cpuScore = scores[comuterChoice(imgCoord)];
+      const diff =  myScore - cpuScore;
+        if(diff === 0 ){
+          setResult('비겼습니다.');
+          setFullCount((prevFullCount) => prevFullCount - 1);
+        }else if([-1,2].includes(diff)){
+          setResult('이겼습니다.');
+          setScore((prevScore) => prevScore + 1);
+          setFullCount((prevFullCount) => prevFullCount - 1);
+        }else {
+          setResult('졌습니다.');
+          setScore((prevScore) => prevScore - 1);
+          setFullCount((prevFullCount) => prevFullCount - 1);
+        }
+        if(fullCount === 0) {
+          setResult('');
+          setScore(0);
+          setFullCount(10);
+          alert(`총 점수는 ${score}점 입니다.`);
+        }
+        setTimeout(() => {
+          setIsRunning(true);
+        },1500)
+    }
+  };
+
+  return(
+  <>
+  <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+  <div>
+    <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
+    <button id="scissor" className="btn" onClick={onClickBtn('가위')}>가위</button>
+    <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
+  </div>
+  <div>{result}</div>
+  <div>현재 {score}점</div>
+  <div>남은 시도 횟수 {fullCount}번</div>
+</>
+  );
+}
+
+
+
+
+export default RSP;
--- 05.가위바위보/client.jsx
+++ 05.가위바위보/client.jsx
@@ -1,7 +1,7 @@
 import React from'react';
 import ReactDOM from'react-dom/client';
 
-import RCP from './RCP.jsx';
+import RCP from './RCP-kustomhook.jsx';
 
 
 //ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
