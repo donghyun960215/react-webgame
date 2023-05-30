3bdbfff1824b020823e460eb8f8454bd3de06f00
donghyun960215 <tlzma789@naver.com>
2023-05-30 15:09:35 +0900

    23.05.30 오타수정

--- 06.로또 추첨기/Lotto.jsx
+++ 06.로또 추첨기/Lotto.jsx
@@ -16,7 +16,7 @@ function getWinNumbers() {
 
 const Lotto = () => {
   const {winNumbers, setWinNumbers} = useState(getWinNumbers());
-  const {winBalls, setWinBalls} = useState();
+  const {winBalls, setWinBalls} = useState([]);
   const {bonus, setBonus} = useState(null);
   const {redo, setRedo} = useState(false);
   const timeouts = useRef([]);
